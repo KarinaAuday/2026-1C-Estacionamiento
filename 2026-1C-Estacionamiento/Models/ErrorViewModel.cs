470d6a169c43e0511aa90c16408b78ab98bbc64e
KARINA AUDAY <karina.auday@ort.edu.ar>
2026-04-06 20:30:26 -0300

    Creacion Proyecto MVC

---
+++ 2026-1C-Estacionamiento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2026_1C_Estacionamiento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
