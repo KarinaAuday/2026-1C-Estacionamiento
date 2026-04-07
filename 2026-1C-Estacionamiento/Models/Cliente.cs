fd72d5a5118226b780abe6d5feeccad724e98a89
KARINA AUDAY <karina.auday@ort.edu.ar>
2026-04-06 22:24:41 -0300

    Agrego Persona y Cliente

---
+++ 2026-1C-Estacionamiento/Models/Cliente.cs
@@ -0,0 +1,7 @@
+﻿namespace _2026_1C_Estacionamiento.Models
+{
+    public class Cliente : Persona
+    {
+        public string Cuit  { get; set; }
+    }
+}
---
+++ 2026-1C-Estacionamiento/Models/Persona.cs
@@ -0,0 +1,24 @@
+﻿namespace _2026_1C_Estacionamiento.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+     
+        public string Foto { get; set; }
+
+        public string NombreCompleto {
+            get
+            {
+                return $"{Nombre} {Apellido}";
+            }
+        }
+
+        
+    }
+}
