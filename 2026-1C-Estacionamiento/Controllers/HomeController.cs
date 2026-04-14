6883038b8965b743ea2a468e5aa1cc14fec62d07
KARINA AUDAY <karina.auday@ort.edu.ar>
2026-04-13 22:47:34 -0300

    Ejemplos paso de Parametros

--- 2026-1C-Estacionamiento/Controllers/HomeController.cs
+++ 2026-1C-Estacionamiento/Controllers/HomeController.cs
@@ -23,6 +23,24 @@ namespace _2026_1C_Estacionamiento.Controllers
             return View();
         }
 
+
+        public IActionResult Pruebas1()
+        {
+            return View();
+        }
+
+        public IActionResult Pruebas2( int num , string nombre , string apellido)
+        {
+           ViewBag.Numero = num;
+           ViewBag.Nombre = nombre;
+           ViewBag.Apellido = apellido;
+            Persona persona = new Persona() { Nombre = nombre, Apellido = apellido };
+            ViewBag.Persona = persona;
+            ViewBag.ListaNombres = new List<string>() { "Juan", "Maria", "Pedro", "Ana" };
+           
+            return View("PasoParametros",persona);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
