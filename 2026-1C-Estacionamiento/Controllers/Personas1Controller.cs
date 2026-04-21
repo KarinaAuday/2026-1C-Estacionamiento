cf64a062096d3a6baf5c2c21085b826faf27a440
KARINA AUDAY <karina.auday@ort.edu.ar>
2026-04-20 22:31:20 -0300

    Ejemplo de Formulario creado a mano con paso de parametros

---
+++ 2026-1C-Estacionamiento/Controllers/Personas1Controller.cs
@@ -0,0 +1,44 @@
+﻿using _2026_1C_Estacionamiento.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2026_1C_Estacionamiento.Controllers
+{
+    public class Personas1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult CrearPersona(string nombre, string apellido)
+        {
+            Persona persona = new Persona
+            {
+                Nombre = nombre,
+                Apellido = apellido
+            };
+            return View("MostrarDatos", persona);
+        }
+
+        public IActionResult CrearGet()
+        {
+            return View();
+        }
+
+        public IActionResult CrearPost()
+        {
+            return View();
+        }
+
+        public IActionResult CrearPersona2(string nombre, string apellido)
+        {
+            // Aquí podrías guardar la persona en una base de datos o realizar alguna acción con los datos recibidos.
+            // Por ahora, simplemente los mostramos en la vista.
+            ViewBag.Nombre = nombre;
+            ViewBag.Apellido = apellido;
+            ViewBag.NombreCompleto = $"{nombre} {apellido}";
+            return View("MostrarDatos2");
+        }
+    }
+}
